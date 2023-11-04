7b956a3656574bb9bf89f59551c1aedb5b16756f
gioimtg2003 <conggioi.pro264@gmail.com>
2023-11-04 12:41:27 +0700

    update all model

---
+++ UI/Model/ExportProductDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Model
+{
+    internal class ExportProductDetail
+    {
+        public int ExportBillDetailID { get; set; }
+        public int Quantity { get; set; }
+        public int StockDetailID { get; set; }
+        public ExportBillDetail ExportBillDetail { get; set; }
+        public StockDetail StockDetail { get; set; }
+    }
+}
